86ed00d000bffd316636977038512fa28e7c8b58
Osnar Couto <83832423+osnarcouto@users.noreply.github.com>
2025-05-09 11:55:48 -0400

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,11 @@
 import pytest
 from unittest.mock import patch
+import os
+
+def pytest_configure():
+    os.environ['BOT_TOKEN'] = '7857806944:AAHd-U9miCS9D5R2ZqlYA33QhT_VC468xro'
+    os.environ['DATABASE_URL'] = 'postgresql://julliuz:Julliuz25Db9203IA@localhost:5432/julliuz_bot'
+    os.environ['REDIS_URL'] = 'redis://localhost:6379/0'
 
 @pytest.fixture(autouse=True)
 def mock_env_vars():
